15791557ac33dd1760596ac0e14aaa2b972f8581
Tiago Tito <tiago.tito.sampaio@gmail.com>
2025-12-05 15:14:48 -0300

    crud completo

--- exercicio express/app.js
+++ exercicio express/app.js
@@ -22,7 +22,7 @@ app.post('/usuarios', (req, res) => {
     }
     else {
         const newUser = {
-            id: ++users.length, 
+            id: users.length + 1, 
             userName: userName, 
             nomeCompleto: nomeCompleto, 
             senha: senha, 
@@ -35,7 +35,48 @@ app.post('/usuarios', (req, res) => {
 });
 
 app.get('/usuarios', (req, res) => {
+    res.status(200).json(users)
+})
 
+app.put('/usuario/:id', (req, res) => {
+    const { userName, nomeCompleto, senha, email} = req.body
+    const index = parseInt(req.params.id)
+    const userIndex = users.findIndex(user => user.id === index)
+
+    if( userIndex < 0) {
+        res.status(400).send('usuário não encontrado')
+    }
+    else if(userName.length < 6 || nomeCompleto.length < 6) {
+        res.status(400).send('o nome de usuário e o nome completo precisam ter pelo menos 6 characteres');
+    }
+    else if(senha.length < 6) {
+        res.status(400).send('a senha precisa ter pelo menos 6 characteres');
+    }
+    else{
+        const updatedUser = {
+            id: userIndex, 
+            userName: userName, 
+            nomeCompleto: nomeCompleto, 
+            senha: senha, 
+            email: email
+        }
+
+        users[userIndex] = updatedUser
+        res.status(200).json(updatedUser)
+    }
+})
+
+app.delete('/usuario/:id', (req, res) => {
+    const index = parseInt(req.params.id)
+    const userIndex = users.findIndex(user => user.id === index)
+
+    if( userIndex < 0) {
+        res.status(400).send('usuário não encontrado')
+    }
+    else{
+        users.splice(userIndex, 1)
+        res.status(200).send(`Usuário de id ${userIndex} deletado`)
+    }
 })
 
 app.listen(3000, () => {
